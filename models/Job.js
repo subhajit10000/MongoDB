d13ffee5fdbde57d48af02aeb66a72cd8a5fc6b1
Subhajit Chakraborty <166530750+subhajit10000@users.noreply.github.com>
2026-07-29 10:43:23 +0530

    Add files via upload

---
+++ models/Job.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const jobSchema = new mongoose.Schema(
+    {
+        title: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        company: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        location: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        description: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+
+        recruiter: {
+            type: mongoose.Schema.Types.ObjectId,
+            ref: "User",
+            required: true,
+        },
+    },
+    {
+        timestamps: true,
+    }
+);
+
+module.exports = mongoose.model(
+    "Job",
+    jobSchema
+);
